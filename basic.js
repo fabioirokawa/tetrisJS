9d5e113faf13e55d96a0f9c8a50f5c78976be1be
Fabio Seiji Irokawa <fabio.irokawa1@hotmail.com>
2019-11-23 02:09:28 -0300

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,19 +1,22 @@
 var stage = document.getElementById('stage');
 var ctx = stage.getContext("2d");
-
+document.addEventListener("keydown", keyPush);
 var level = [5][5] = 0;
 
-var sqrX = 10;
-var sqrY = 10;
+var sqrX = 51;
+var sqrY = 51;
 
+const StartSpeed = 800;
+var speed = 800;
 
 window.onload = function () {
-    setInterval(game, 80);
+    setInterval(game, 50);
+    setInterval(this.fall_speed, speed);
 }
 
 function game() {
     //sqrX = --sqrX;
-    sqrY = ++sqrY;
+    //sqrY = ++sqrY;
 
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, stage.width, stage.height);
@@ -21,14 +24,38 @@ function game() {
     drawBoard();
 
     ctx.fillStyle = "red";
-    ctx.fillRect(sqrX * 5, sqrY * 5, 100, 100);
+    ctx.fillRect(sqrX, sqrY, 39, 39); //não usar número no lugar do sqrX e sqrY
+
+}
+
+async function fall_speed(){
+    sqrY = sqrY + 40;
+}
+
+function keyPush(event){
+    switch (event.keyCode) {
+        case 37: // Left
+            sqrX = sqrX - 40;
+            break;
+        case 38: // up
+            sqrY = sqrY + 100;
+            break;
+        case 39: // right
+            sqrX = sqrX + 40;
+            break;
+        case 40: // down
+            sqrX = --sqrX;
+            break;			
+        default:
+            break;
+    }
 }
 
 function drawBoard() {
     // Box width
     var bw = 400;
     // Box height
-    var bh = 400;
+    var bh = 600;
     // Padding
     var p = 50;
 
